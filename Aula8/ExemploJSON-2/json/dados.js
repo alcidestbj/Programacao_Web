8d1dace66ba383dcc8024fc3bcfc9edba3c1cf84
AlcidesJr <8090502+alcidestbj@users.noreply.github.com>
2019-10-18 23:39:10 -0300

    Add files via upload

---
+++ Aula8/ExemploJSON-2/json/dados.js
@@ -0,0 +1,48 @@
+var alunos = [
+    {
+        "rgm": "123456-0",
+        "nome": "Fulano",
+        "idade": 22
+    },
+    {
+        "rgm": "988985-7",
+        "nome": "Beltrano",
+        "idade": 30
+    },
+    {
+        "rgm": "232323-4",
+        "nome": "Ciclano",
+        "idade": 25
+    },
+    {
+        "rgm": "444444-4",
+        "nome": "Corona",
+        "idade": 21
+    }
+]
+var prog = [
+    {
+        "numero": 5,
+        "tipo": "Aberto",
+        "nome": "Rede Globo",
+        "noticia": [{
+            "dataPublicacao": "30/10/2016",
+            "titulo": "Segundo turno 2016",
+            "link": "http://www.globo.com"
+        }, {
+            "dataPublicacao": "29/10/2016",
+            "titulo": "Falta de remédio nos postos",
+            "link": "http://www.globo.com"
+        }, ]
+    },
+    {
+        "numero": 22,
+        "tipo": "Assinatura",
+        "nome": "SportTV",
+        "noticia": [{
+            "dataPublicacao": "27/10/2016",
+            "titulo": "Superliga feminina",
+            "link": "http://sportv.globo.com/videos/superliga"
+        }]
+    }
+]
